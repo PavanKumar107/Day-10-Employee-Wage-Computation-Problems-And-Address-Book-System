bc816a8d6697d6cf73b8f7d8522a4866b789460d
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-25 09:40:46 +0530

    UC10

---
+++ Oops Employee Wage-10/src/employeewage/UC10a.java
@@ -0,0 +1,26 @@
+package employeewage;
+
+import java.util.ArrayList;
+
+public class UC10a {
+	String name;
+	int monthlyWage;
+
+	void totalWage(String name,int monthlyWage) {
+		this.name=name;
+		this.monthlyWage=monthlyWage;
+	}
+	static ArrayList<UC10> companyList=new ArrayList<UC10>();
+
+	void showInfo(UC10 company) {
+		System.out.println("Company name: "+company.name);
+		System.out.println("Total Wage :"+company.monthlyWage);
+	}
+	void showInfo() {
+		System.out.println("Company name: "+name);
+		System.out.println("Total Wage :"+monthlyWage);
+	}
+	void addCompany(UC10 company) {
+		companyList.add(company);
+	}
+}
