04af583e65f6773c295629e306fa003e0204555e
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-25 09:35:46 +0530

    AB-6

---
+++ Address Book System-6/src/addressbook/BookList.java
@@ -0,0 +1,34 @@
+package addressbook;
+public class BookList {
+
+	public int checkBook(String bookName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void addBook(String bookName, UC6 addressBook) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addBook1(String bookName, UC6 addressBook) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addBook11(String bookName, UC6 addressBook) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int checkBook1(String bookName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int checkBook11(String bookName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Address Book System-6/src/addressbook/Contact.java
@@ -0,0 +1,17 @@
+package addressbook;
+
+public class Contact {
+	public Object firstName;
+
+	public void addContact1() {
+		// TODO Auto-generated method stub
+	}
+
+	public void addContact() {
+		// TODO Auto-generated method stub
+	}
+
+	public void addContact11() {
+		// TODO Auto-generated method stub
+	}
+}
